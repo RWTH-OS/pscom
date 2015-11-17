bc14fcbae20f8ecb83a9f46e1261d691226ab36b
JonBau <JonBau@users.noreply.github.com>
2015-11-17 15:02:37 +0100

    Test20

---
+++ lib/pscom4ivshmem/pscom_openib.c
@@ -0,0 +1,617 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2002-2004 ParTec AG, Karlsruhe
+ * Copyright (C) 2005-2007 ParTec Cluster Competence Center GmbH, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ *
+ * Author:	Jens Hauke <hauke@par-tec.com>
+ */
+/**
+ * pscom_openib.c: OPENIB/Infiniband communication
+ */
+
+#include "psoib.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <malloc.h>
+#include <infiniband/verbs.h>
+
+#include "pscom_priv.h"
+#include "pscom_io.h"
+#include "pscom_openib.h"
+#include "pscom_req.h"
+#include "pscom_util.h"
+
+
+static
+struct pscom_poll_reader pscom_cq_poll;
+
+int pscom_poll_cq(pscom_poll_reader_t *reader)
+{
+	psoib_progress();
+
+	if (!psoib_outstanding_cq_entries) {
+		/* Stop polling on cq */
+		/* it's save to dequeue more then once */
+		list_del_init(&reader->next);
+	}
+
+	return 0;
+}
+
+static inline
+void pscom_check_cq_poll(void)
+{
+	if (psoib_outstanding_cq_entries &&
+	    list_empty(&pscom_cq_poll.next)) {
+		// There are outstanding cq events and
+		// we do not already poll the cq
+
+		// Start polling:
+		list_add_tail(&pscom_cq_poll.next, &pscom.poll_reader);
+	}
+}
+
+
+static
+int _pscom_openib_do_read(pscom_con_t *con, psoib_con_info_t *mcon)
+{
+	void *buf;
+	int size;
+
+	size = psoib_recvlook(mcon, &buf);
+
+	if (size >= 0) {
+		perf_add("openib_do_read");
+		pscom_read_done(con, buf, size);
+
+		psoib_recvdone(mcon);
+		return 1;
+	} else if ((size == -EINTR) || (size == -EAGAIN)) {
+		// Nothing received
+		return 0;
+	} else {
+		// Error
+		errno = -size;
+		pscom_con_error(con, PSCOM_OP_READ, PSCOM_ERR_STDERROR);
+		return 1;
+	}
+}
+
+
+static
+int pscom_openib_do_read(pscom_poll_reader_t *reader)
+{
+	pscom_con_t *con = list_entry(reader, pscom_con_t, poll_reader);
+	psoib_con_info_t *mcon = con->arch.openib.mcon;
+
+	return _pscom_openib_do_read(con, mcon);
+}
+
+
+static
+void pscom_openib_do_write(pscom_con_t *con)
+{
+	unsigned int len;
+	struct iovec iov[2];
+	pscom_req_t *req;
+
+	req = pscom_write_get_iov(con, iov);
+
+	if (req) {
+		psoib_con_info_t *mcon = con->arch.openib.mcon;
+		len = iov[0].iov_len + iov[1].iov_len;
+
+		perf_add("openib_sendv");
+		int rlen = psoib_sendv(mcon, iov, len);
+
+		if (rlen >= 0) {
+			pscom_write_done(con, req, rlen);
+			pscom_check_cq_poll();
+		} else if ((rlen == -EINTR) || (rlen == -EAGAIN)) {
+			// Busy: Maybe out of tokens? try to read more tokens:
+			_pscom_openib_do_read(con, mcon);
+		} else {
+			// Error
+			pscom_con_error(con, PSCOM_OP_WRITE, PSCOM_ERR_STDERROR);
+		}
+	}
+}
+
+
+/*
+ * ++ RMA rendezvous begin
+ */
+#ifdef IB_USE_RNDV
+
+typedef struct pscom_rendezvous_data_openib {
+	struct psoib_rma_req	rma_req;
+	pscom_req_t		*rendezvous_req; // Receiving side: users receive request (or generated request)
+	pscom_con_t		*con;
+	void			(*io_done)(void *priv);
+	void			*priv;
+} pscom_rendezvous_data_openib_t;
+
+
+static inline
+pscom_rendezvous_data_openib_t *get_req_data(pscom_rendezvous_data_t *rd)
+{
+	_pscom_rendezvous_data_openib_t *data = &rd->arch.openib;
+	pscom_rendezvous_data_openib_t *res = (pscom_rendezvous_data_openib_t *) data;
+	assert(sizeof(*res) <= sizeof(*data));
+	return res;
+}
+
+
+static
+unsigned int pscom_openib_rma_mem_register(pscom_con_t *con, pscom_rendezvous_data_t *rd)
+{
+	int err = 0;
+	pscom_rendezvous_data_openib_t *openib_rd = get_req_data(rd);
+	psoib_con_info_t *ci = con->arch.openib.mcon;
+	psoib_rma_mreg_t *mreg = &openib_rd->rma_req.mreg;
+
+#ifdef IB_RNDV_USE_PADDING
+#ifdef   IB_RNDV_RDMA_WRITE
+#error   IB_RNDV_USE_PADDING and IB_RNDV_RDMA_WRITE are mutually exclusive!
+#endif
+
+	rd->msg.arch.openib.padding_size = (IB_RNDV_PADDING_SIZE - ((long long int)rd->msg.data) % IB_RNDV_PADDING_SIZE) % IB_RNDV_PADDING_SIZE;
+
+	memcpy(rd->msg.arch.openib.padding_data, rd->msg.data, rd->msg.arch.openib.padding_size);
+
+	/* get mem region */
+	perf_add("openib_acquire_rma_mreg");
+	err = psoib_acquire_rma_mreg(mreg, rd->msg.data + rd->msg.arch.openib.padding_size, rd->msg.data_len - rd->msg.arch.openib.padding_size, ci);
+	assert(!err);
+
+	if (err) goto err_get_region;
+
+	rd->msg.arch.openib.mr_key  = mreg->mem_info.mr->rkey;
+	rd->msg.arch.openib.mr_addr = (uint64_t)mreg->mem_info.ptr;
+
+	return sizeof(rd->msg.arch.openib) - sizeof(rd->msg.arch.openib.padding_data) + rd->msg.arch.openib.padding_size;
+#else
+
+	/* get mem region */
+	perf_add("openib_acquire_rma_mreg2");
+	err = psoib_acquire_rma_mreg(mreg, rd->msg.data, rd->msg.data_len, ci);
+	assert(!err);
+
+	if (err) goto err_get_region;
+
+	rd->msg.arch.openib.mr_key  = mreg->mem_info.mr->rkey;
+	rd->msg.arch.openib.mr_addr = (uint64_t)mreg->mem_info.ptr;
+
+	return sizeof(rd->msg.arch.openib) - sizeof(rd->msg.arch.openib.padding_data);
+#endif
+
+err_get_region:
+	// ToDo: Handle Errors!
+	return 0;
+}
+
+
+static
+void pscom_openib_rma_mem_deregister(pscom_con_t *con, pscom_rendezvous_data_t *rd)
+{
+	pscom_rendezvous_data_openib_t *openib_rd = get_req_data(rd);
+	psoib_rma_mreg_t *mreg = &openib_rd->rma_req.mreg;
+
+	perf_add("openib_release_rma_mreg");
+	psoib_release_rma_mreg(mreg);
+}
+
+
+static
+void pscom_openib_rma_read_io_done(void *priv, int err)
+{
+	psoib_rma_req_t *dreq = (psoib_rma_req_t *)priv;
+	pscom_rendezvous_data_openib_t *psopenib_rd =
+		(pscom_rendezvous_data_openib_t *)dreq->priv;
+
+	pscom_req_t *rendezvous_req = psopenib_rd->rendezvous_req;
+	psoib_rma_mreg_t *mreg = &psopenib_rd->rma_req.mreg;
+
+	psoib_release_rma_mreg(mreg);
+
+	if (unlikely(err)) {
+		rendezvous_req->pub.state |= PSCOM_REQ_STATE_ERROR;
+	}
+	_pscom_recv_req_done(rendezvous_req);
+}
+
+
+static
+int pscom_openib_rma_read(pscom_req_t *rendezvous_req, pscom_rendezvous_data_t *rd)
+{
+	int err, ret;
+	pscom_rendezvous_data_openib_t *psopenib_rd = get_req_data(rd);
+	psoib_rma_req_t *dreq = &psopenib_rd->rma_req;
+	pscom_con_t *con = get_con(rendezvous_req->pub.connection);
+	psoib_con_info_t *ci = con->arch.openib.mcon;
+
+	perf_add("openib_rma_read");
+#ifdef IB_RNDV_USE_PADDING
+	memcpy(rendezvous_req->pub.data, rd->msg.arch.openib.padding_data, rd->msg.arch.openib.padding_size);
+	rendezvous_req->pub.data += rd->msg.arch.openib.padding_size;
+	rendezvous_req->pub.data_len -= rd->msg.arch.openib.padding_size;
+#endif
+
+	err = psoib_acquire_rma_mreg(&dreq->mreg, rendezvous_req->pub.data, rendezvous_req->pub.data_len, ci);
+	assert(!err); // ToDo: Catch error
+
+	dreq->remote_addr = rd->msg.arch.openib.mr_addr;
+	dreq->remote_key  = rd->msg.arch.openib.mr_key;
+	dreq->data_len = rendezvous_req->pub.data_len;
+	dreq->ci = ci;
+	dreq->io_done = pscom_openib_rma_read_io_done;
+	dreq->priv = psopenib_rd;
+
+	psopenib_rd->rendezvous_req = rendezvous_req;
+
+	err = psoib_post_rma_get(dreq);
+	assert(!err); // ToDo: Catch error
+
+	return 0;
+}
+
+
+static
+void pscom_openib_rma_write_io_done(void *priv, int err)
+{
+	pscom_rendezvous_data_t *rd_data = (pscom_rendezvous_data_t *)priv;
+	pscom_rendezvous_data_openib_t *rd_data_openib = get_req_data(rd_data);
+
+	// ToDo: Error propagation
+	rd_data_openib->io_done(rd_data_openib->priv);
+
+	pscom_openib_rma_mem_deregister(rd_data_openib->con, rd_data);
+	pscom_free(rd_data);
+}
+
+
+/* Send from:
+ *   rd_src = (pscom_rendezvous_data_t *)req->pub.user
+ *   (rd_src->msg.data, rd_src->msg.data_len)
+ *   rd_src->msg.arch.openib.{mr_key, mr_addr}
+ * To:
+ *   (rd_des->msg.data, rd_des->msg.data_len)
+ *   rd_des->msg.arch.openib.{mr_key, mr_addr}
+ */
+
+static
+int pscom_openib_rma_write(pscom_con_t *con, void *src, pscom_rendezvous_msg_t *des,
+			   void (*io_done)(void *priv), void *priv)
+{
+	pscom_rendezvous_data_t *rd_data = (pscom_rendezvous_data_t *)pscom_malloc(sizeof(*rd_data));
+	pscom_rendezvous_data_openib_t *rd_data_openib = get_req_data(rd_data);
+	psoib_con_info_t *mcon = con->arch.openib.mcon;
+
+	psoib_rma_req_t *dreq = &rd_data_openib->rma_req;
+	int len, err;
+
+	rd_data->msg.id = (void*)42;
+	rd_data->msg.data = src;
+	rd_data->msg.data_len = des->data_len;
+
+	len = pscom_openib_rma_mem_register(con, rd_data);
+	assert(len); // ToDo: Catch error
+/*
+	dreq->mreg.mem_info.ptr = xxx;
+	dreq->mreg.size = xxx;
+	dreq->mreg.mem_ingo.mr->lkey = xxx;
+*/
+	perf_add("openib_rma_write");
+
+	dreq->remote_addr = des->arch.openib.mr_addr;
+	dreq->remote_key  = des->arch.openib.mr_key;
+	dreq->data_len = des->data_len;
+	dreq->ci = mcon;
+	dreq->io_done = pscom_openib_rma_write_io_done;
+	dreq->priv = rd_data;
+
+	rd_data_openib->con = con;
+	rd_data_openib->io_done = io_done;
+	rd_data_openib->priv = priv;
+
+	err = psoib_post_rma_put(dreq);
+	assert(!err); // ToDo: Catch error
+	rd_data = NULL; /* Do not use rd_data after psoib_post_rma_put()!
+			   io_done might already be called and freed rd_data. */
+
+	return 0;
+}
+#endif
+/*
+ * -- RMA rendezvous end
+ */
+
+
+static
+void pscom_openib_close(pscom_con_t *con)
+{
+	psoib_con_info_t *mcon = con->arch.openib.mcon;
+
+	if (!mcon) return;
+
+	psoib_send_eof(mcon);
+
+	psoib_con_cleanup(mcon, NULL);
+	psoib_con_free(mcon);
+
+	con->arch.openib.mcon = NULL;
+}
+
+#ifdef IB_USE_RNDV
+#ifdef IB_RNDV_USE_MALLOC_HOOKS
+static void *pscom_openib_morecore_hook(ptrdiff_t incr)
+{
+	/* Do not return memory back to the OS: (do not trim) */
+	if(incr < 0) {
+		return __default_morecore(0);
+	} else {
+		return __default_morecore(incr);
+	}
+}
+
+static void pscom_openib_free_hook(void *ptr, const void *caller)
+{
+	static void *(*old_malloc_hook)(size_t, const void *);
+	static void (*old_free_hook)(void *, const void *);
+
+	/* !!! __malloc_hook and __free_hook are deprecated !!! */
+
+	__malloc_hook = old_malloc_hook;
+	__free_hook = old_free_hook;
+
+	/* !!! TODO: Check registration cache !!! */
+
+	/* Call recursively */
+	free (ptr);
+
+	/* Save underlying hooks */
+	old_malloc_hook = __malloc_hook;
+	old_free_hook = __free_hook;
+}
+#endif
+#endif
+
+
+static
+void pscom_openib_con_init(pscom_con_t *con, int con_fd,
+			   psoib_con_info_t *mcon)
+{
+	con->pub.state = PSCOM_CON_STATE_RW;
+	con->pub.type = PSCOM_CON_TYPE_OPENIB;
+
+	close(con_fd);
+
+	con->arch.openib.mcon = mcon;
+
+	// Only Polling:
+	con->write_start = pscom_poll_write_start;
+	con->write_stop = pscom_poll_write_stop;
+	con->read_start = pscom_poll_read_start;
+	con->read_stop = pscom_poll_read_stop;
+
+	con->poll_reader.do_read = pscom_openib_do_read;
+	con->do_write = pscom_openib_do_write;
+	con->close = pscom_openib_close;
+
+#ifdef IB_USE_RNDV
+	con->rma_mem_register = pscom_openib_rma_mem_register;
+	con->rma_mem_deregister = pscom_openib_rma_mem_deregister;
+#ifdef IB_RNDV_RDMA_WRITE
+	con->rma_write = pscom_openib_rma_write;
+#else
+	con->rma_read = pscom_openib_rma_read;
+#endif
+
+	con->rendezvous_size = pscom.env.rendezvous_size_openib;
+
+#ifdef IB_RNDV_DISABLE_FREE_TO_OS
+
+	/* We have to prevent free() from returning memory back to the OS: */
+
+#ifndef IB_RNDV_USE_MALLOC_HOOKS
+	if (con->rendezvous_size != ~0U) {
+		/* See 'man mallopt(3) / M_MMAP_MAX': Setting this parameter to 0 disables the use of mmap(2) for servicing large allocation requests. */
+		mallopt(M_MMAP_MAX, 0);
+
+		/* See 'man mallopt(3) / M_TRIM_THRESHOLD': Setting M_TRIM_THRESHOLD to -1 disables trimming completely. */
+		mallopt(M_TRIM_THRESHOLD, -1);
+	}
+#else
+	if(__morecore == __default_morecore) {
+		/* Switch to our own function pscom_openib_morecore() that does not trim: */
+		__morecore = pscom_openib_morecore_hook;
+	}
+
+	__free_hook = pscom_openib_free_hook;
+#endif
+
+#endif
+#endif
+}
+
+/*********************************************************************/
+static
+void pscom_openib_init(void)
+{
+	psoib_debug = pscom.env.debug;
+	psoib_debug_stream = pscom_debug_stream();
+	pscom_env_get_str(&psoib_hca, ENV_OPENIB_HCA);
+	pscom_env_get_uint(&psoib_port, ENV_OPENIB_PORT);
+	pscom_env_get_uint(&psoib_path_mtu, ENV_OPENIB_PATH_MTU);
+
+	pscom_env_get_uint(&psoib_recvq_size, ENV_OPENIB_RECVQ_SIZE);
+
+	pscom_env_get_int(&psoib_global_sendq, ENV_OPENIB_GLOBAL_SENDQ);
+	pscom_env_get_uint(&psoib_compq_size, ENV_OPENIB_COMPQ_SIZE);
+	if (psoib_global_sendq) {
+		// One sendq for all connection. limit sendq to compq size.
+		psoib_sendq_size = psoib_compq_size;
+	} else {
+		// One sendq for each connection. limit sendq to recvq size.
+		psoib_sendq_size = pscom_min(psoib_sendq_size, psoib_recvq_size);
+	}
+	pscom_env_get_uint(&psoib_sendq_size, ENV_OPENIB_SENDQ_SIZE);
+
+	psoib_pending_tokens = psoib_pending_tokens_suggestion();
+	pscom_env_get_uint(&psoib_pending_tokens, ENV_OPENIB_PENDING_TOKENS);
+
+//	if (!psoib_global_sendq && psoib_sendq_size == psoib_recvq_size) {
+//		// Disable event counting:
+//		psoib_event_count = 0;
+//	}
+	pscom_env_get_int(&psoib_event_count, ENV_OPENIB_EVENT_CNT);
+	pscom_env_get_int(&psoib_ignore_wrong_opcodes, ENV_OPENIB_IGNORE_WRONG_OPCODES);
+	pscom_env_get_int(&psoib_lid_offset, ENV_OPENIB_LID_OFFSET);
+
+	INIT_LIST_HEAD(&pscom_cq_poll.next);
+	pscom_cq_poll.do_read = pscom_poll_cq;
+
+}
+
+
+static
+int pscom_openib_connect(pscom_con_t *con, int con_fd)
+{
+	int arch = PSCOM_ARCH_OPENIB;
+	psoib_con_info_t *mcon = psoib_con_create();
+	psoib_info_msg_t msg;
+	int call_cleanup_con = 0;
+	int err;
+
+	if (psoib_init() || !mcon)
+		goto dont_use;  /* Dont use openib */
+
+	/* We want talk openib */
+	pscom_writeall(con_fd, &arch, sizeof(arch));
+
+	/* step 1 */
+	if ((pscom_readall(con_fd, &arch, sizeof(arch)) != sizeof(arch)) ||
+	    (arch != PSCOM_ARCH_OPENIB))
+		goto err_remote;
+
+	/* step 2 : recv connection id's */
+	if ((pscom_readall(con_fd, &msg, sizeof(msg)) != sizeof(msg)))
+		goto err_remote;
+
+	err = psoib_con_init(mcon, NULL, NULL);
+	if (!err) {
+		call_cleanup_con = 1;
+		err = psoib_con_connect(mcon, &msg);
+	}
+
+	/* step 3 : send connection id's (or error) */
+	if (!err) {
+		psoib_con_get_info_msg(mcon, &msg);
+	} else {
+		msg.lid = 0xffff; // send error
+	}
+
+	pscom_writeall(con_fd, &msg, sizeof(msg));
+
+	if (err) goto err_connect;
+
+	/* step 4: openib initialized. Recv final ACK. */
+	if ((pscom_readall(con_fd, &msg, sizeof(msg)) != sizeof(msg)) ||
+	    (msg.lid == 0xffff)) goto err_ack;
+
+	pscom_openib_con_init(con, con_fd, mcon);
+
+	return 1;
+	/* --- */
+err_ack:
+err_connect:
+	if (call_cleanup_con) psoib_con_cleanup(mcon, NULL);
+err_remote:
+dont_use:
+	if (mcon) psoib_con_free(mcon);
+	return 0;
+}
+
+
+static
+int pscom_openib_accept(pscom_con_t *con, int con_fd)
+{
+	int arch = PSCOM_ARCH_OPENIB;
+	psoib_con_info_t *mcon = NULL;
+	psoib_info_msg_t msg;
+
+	if (psoib_init())
+		goto out_noopenib;
+
+	mcon = psoib_con_create();
+	if (!mcon)
+		goto out_noopenib;
+
+	if (psoib_con_init(mcon, NULL, NULL))
+		goto err_con_init;
+
+	/* step 1:  Yes, we talk openib. */
+	pscom_writeall(con_fd, &arch, sizeof(arch));
+
+	/* step 2: Send Connection id's */
+	psoib_con_get_info_msg(mcon, &msg);
+
+	pscom_writeall(con_fd, &msg, sizeof(msg));
+
+	/* step 3 : recv connection id's */
+	if ((pscom_readall(con_fd, &msg, sizeof(msg)) != sizeof(msg)) ||
+	    (msg.lid == 0xffff))
+		goto err_remote;
+
+	if (psoib_con_connect(mcon, &msg))
+		goto err_connect_con;
+
+	/* step 4: OPENIB mem initialized. Send final ACK. */
+	msg.lid = 0;
+	pscom_writeall(con_fd, &msg, sizeof(msg));
+
+	pscom_openib_con_init(con, con_fd, mcon);
+
+	return 1;
+	/* --- */
+err_connect_con:
+	/* Send NACK */
+	msg.lid = 0xffff;
+	pscom_writeall(con_fd, &msg, sizeof(msg));
+err_remote:
+	psoib_con_cleanup(mcon, NULL);
+err_con_init:
+out_noopenib:
+	if (mcon) psoib_con_free(mcon);
+	arch = PSCOM_ARCH_ERROR;
+	pscom_writeall(con_fd, &arch, sizeof(arch));
+	return 0; /* Dont use openib */
+	/* --- */
+}
+
+
+pscom_plugin_t pscom_plugin = {
+	.name		= "openib",
+	.version	= PSCOM_PLUGIN_VERSION,
+	.arch_id	= PSCOM_ARCH_OPENIB,
+	.priority	= PSCOM_OPENIB_PRIO,
+
+	.init		= pscom_openib_init,
+	.destroy	= NULL,
+	.sock_init	= NULL,
+	.sock_destroy	= NULL,
+	.con_connect	= pscom_openib_connect,
+	.con_accept	= pscom_openib_accept,
+};
---
+++ lib/pscom4ivshmem/psoib.h
@@ -0,0 +1,160 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2007 ParTec Cluster Competence Center GmbH, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ *
+ * Author:	Jens Hauke <hauke@par-tec.com>
+ */
+/**
+ * psoib.c: OPENIB/Infiniband communication
+ */
+
+#ifndef _PSOIB_H_
+#define _PSOIB_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/uio.h>
+#include "list.h"
+
+typedef struct psoib_con_info psoib_con_info_t;
+typedef struct hca_info hca_info_t;
+typedef struct port_info port_info_t;
+
+// contact endpoint info
+typedef struct psoib_info_msg_s {
+	uint16_t	lid;
+	uint32_t	qp_num;  /* QP number */
+	void		*remote_ptr; /* Info about receive buffers */
+	uint32_t	remote_rkey;
+} psoib_info_msg_t;
+
+
+typedef struct {
+    void *ptr;
+    struct ibv_mr *mr;
+} mem_info_t;
+
+#define IB_USE_RNDV
+#define IB_RNDV_RDMA_WRITE
+#define IB_RNDV_THRESHOLD 4096
+#define IB_RNDV_USE_MREG_CACHE
+#define IB_RNDV_MREG_CACHE_SIZE 256
+#define IB_RNDV_DISABLE_FREE_TO_OS
+#undef  IB_RNDV_USE_MALLOC_HOOKS
+/* Use IB_RNDV_USE_PADDING not together with IB_RNDV_RDMA_WRITE! */
+// #define IB_RNDV_USE_PADDING
+#define IB_RNDV_PADDING_SIZE 64
+/* IB_RNDV_PADDING_SIZE must not be bigger than 64 (or adjust pscom_priv.h respectively!) */
+
+#if defined(IB_USE_RNDV) && defined(IB_DONT_USE_ZERO_COPY)
+#undef IB_USE_RNDV
+#endif
+
+#if defined(IB_RNDV_USE_MREG_CACHE)
+/* if we use a registration cache, we _have_ to disable free() returning memory to the OS: */
+#define IB_RNDV_DISABLE_FREE_TO_OS
+#endif
+
+/*
+ * ++ RMA rendezvous
+ */
+#ifdef IB_USE_RNDV
+/* registered memory region. (Opaque object for users of psoib_get_rma_mreg() and psoib_put_rma_mreg()) */
+typedef struct psoib_rma_req psoib_rma_req_t;
+
+typedef struct psoib_rma_mreg {
+	mem_info_t      mem_info;
+	size_t          size;
+#ifdef IB_RNDV_USE_MREG_CACHE
+	struct psoib_mregion_cache* mreg_cache;
+#endif
+} psoib_rma_mreg_t;
+
+
+/* rendezvous data for the rma get request */
+struct psoib_rma_req {
+	struct list_head next;
+	size_t		 data_len;
+	psoib_rma_mreg_t  mreg;
+	psoib_con_info_t *ci;
+	uint32_t        remote_key;
+	uint64_t        remote_addr;
+	void		(*io_done)(void *priv, int err);
+	void		*priv;
+};
+
+int psoib_acquire_rma_mreg(psoib_rma_mreg_t *mreg, void *buf, size_t size, psoib_con_info_t *ci);
+int psoib_release_rma_mreg(psoib_rma_mreg_t *mreg);
+int psoib_post_rma_get(psoib_rma_req_t *req);
+int psoib_post_rma_put(psoib_rma_req_t *req);
+#ifdef IB_RNDV_USE_MREG_CACHE
+void psoib_mregion_cache_cleanup(void);
+void psoib_mregion_cache_init(void);
+#endif
+
+#endif
+/*
+ *  -- RMA rendezvous end
+ */
+
+
+int psoib_init(void);
+
+
+// Connection handling:
+
+psoib_con_info_t *psoib_con_create(void);
+void	psoib_con_free(psoib_con_info_t *con_info);
+
+int	psoib_con_init(psoib_con_info_t *con_info, hca_info_t *hca_info, port_info_t *port_info);
+int	psoib_con_connect(psoib_con_info_t *con_info, psoib_info_msg_t *info_msg);
+void	psoib_con_cleanup(psoib_con_info_t *con_info, hca_info_t *hca_info);
+
+void	psoib_con_get_info_msg(psoib_con_info_t *con_info /* in */, psoib_info_msg_t *info /* out */);
+
+
+/* returnvalue like read() , except on error errno is negative return */
+int psoib_recvlook(psoib_con_info_t *con_info, void **buf);
+void psoib_recvdone(psoib_con_info_t *con_info);
+
+
+/* returnvalue like write(), except on error errno is negative return */
+/* It's important, that the sending side is aligned to IB_MTU_SPEC,
+   else we loose a lot of performance!!! */
+int psoib_sendv(psoib_con_info_t *con_info, struct iovec *iov, int size);
+void psoib_send_eof(psoib_con_info_t *con_info);
+
+/* Handle outstanding cq events. */
+void psoib_progress(void);
+
+/* Suggest a value for psoib_pending_tokens. Result depends on psoib_recvq_size. */
+unsigned psoib_pending_tokens_suggestion(void);
+
+/*
+ * Configuration
+ */
+extern int psoib_debug;
+extern FILE *psoib_debug_stream; /* Stream to use for debug output */
+extern char *psoib_hca; /* hca name to use. Default: first hca */
+extern unsigned int psoib_port; /* port index to use. Default: 0 (means first active port) */
+extern unsigned int psoib_path_mtu; /* path mtu to use. */
+extern unsigned int psoib_sendq_size;
+extern unsigned int psoib_recvq_size;
+extern unsigned int psoib_compq_size;
+extern unsigned int psoib_pending_tokens;
+extern int psoib_global_sendq; /* bool. Use one sendqueue for all connections? */
+extern int psoib_event_count; /* bool. Be busy if outstanding_cq_entries is to high? */
+extern int psoib_ignore_wrong_opcodes; /* bool: ignore wrong cq opcodes */
+extern int psoib_lid_offset; /* int: offset to base LID (adaptive routing) */
+
+/*
+ * Information
+ */
+extern unsigned psoib_outstanding_cq_entries; /* counter */
+
+#endif /* _PSOIB_H_ */
